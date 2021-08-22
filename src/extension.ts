83bb34ab7e9f5de00543bac2d657b2cff1853b0d
xarantolus <xarantolus@protonmail.com>
2021-08-22 10:21:01 +0200

    Add basic hover card provider

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,10 @@
 import * as vscode from 'vscode';
 
+import { HoverProvider } from './hover/provider';
 
 export function activate(context: vscode.ExtensionContext) {
 
+    var disposable = vscode.languages.registerHoverProvider('memeasm', new HoverProvider())
+
+    context.subscriptions.push(disposable);
 }
---
+++ src/hover/provider.ts
@@ -0,0 +1,93 @@
+import * as vscode from 'vscode';
+import { Hover } from 'vscode';
+import { Registry, parseRawGrammar, IRawGrammar, IGrammar, IToken } from "vscode-textmate";
+
+const { promises: { readFile } } = require("fs");
+
+import { readFileSync } from "fs";
+
+import { join } from "path";
+
+import * as oniguruma from 'vscode-oniguruma'
+
+
+export class HoverProvider implements vscode.HoverProvider {
+    private _registry: Registry;
+    private _grammar: IGrammar | null | undefined = null;
+
+    constructor() {
+        const wasmBin = readFileSync(join(__dirname, '../../node_modules/vscode-oniguruma/release/onig.wasm')).buffer;
+
+        const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+            return {
+                createOnigScanner(patterns: string[]) {
+                    return new oniguruma.OnigScanner(patterns);
+                },
+                createOnigString(s: string) {
+                    return new oniguruma.OnigString(s);
+                }
+            };
+        });
+
+        this._registry = new Registry({
+            onigLib: vscodeOnigurumaLib,
+
+            loadGrammar: (scopeName) => {
+                if (scopeName === 'source.memeasm') {
+                    return readFile(join(__dirname, '../../syntaxes/memeasm.tmLanguage.json')).then((data: any) => parseRawGrammar(data.toString(), "memeasm.tmLanguage.json"))
+                }
+                console.log(`Unknown scope name: ${scopeName}`);
+                return null;
+            }
+        })
+
+        this._registry.loadGrammar('source.memeasm').then(g => this._grammar = g)
+    }
+
+
+    provideHover(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+        if (!this._grammar) {
+            return;
+        }
+
+        var line = document.lineAt(position.line);
+
+        var tokenized = this._grammar?.tokenizeLine(line.text, null)
+        if (tokenized.tokens.length == 0) {
+            return;
+        }
+
+
+        var possibleTokens = tokenized.tokens.filter((t: IToken) => {
+            return t.startIndex <= position.character && t.endIndex > position.character;
+        });
+        if (possibleTokens.length === 0) {
+            return;
+        }
+
+        console.log(possibleTokens)
+
+        // TODO: Maybe find only tokens with same scopes length as the one at position
+        // Now find all tokens with entity.name.function scope and find the min/max index
+        var funcTokens = possibleTokens.filter((t: IToken) => {
+            return !t.scopes.includes("entity.name.function")
+        })
+
+        var tokenStart = line.firstNonWhitespaceCharacterIndex, tokenEnd = line.text.length;
+        funcTokens.forEach((t: IToken) => {
+            if (t.startIndex > tokenStart) {
+                tokenStart = t.startIndex;
+            }
+            if (t.endIndex > tokenEnd) {
+                tokenEnd = t.endIndex;
+            }
+        })
+
+        var tokenRange = new vscode.Range(
+            new vscode.Position(position.line, tokenStart),
+            new vscode.Position(position.line, tokenEnd));
+
+
+        return new Hover(line.text.substring(tokenStart, tokenEnd), tokenRange);
+    }
+}
